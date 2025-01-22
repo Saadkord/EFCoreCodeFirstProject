d6beffc065fc5d6499f45b13dd4f73066df63003
Saadkord <67105662+Saadkord@users.noreply.github.com>
2025-01-22 22:24:36 +0200

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using EFCore.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+
+
+namespace EFCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instractors { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Sechedual> Secheduals { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<SectionStudent> SectionStudents { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var config = new ConfigurationBuilder().AddJsonFile("AppSettings.Json").Build();
+            var constr = config.GetSection("constr").Value;
+            optionsBuilder.UseSqlServer(constr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using EFCore.Data;
+using EFCore.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var Context = new AppDbContext())
+            {
+
+                var sections = Context.Sections.Include(x => x.Course).Include(x => x.Instractor).Include(x => x.Secheduals);
+                foreach (var section in sections)
+                {
+                    var sun = section.Secheduals.SUN ? "x" : "";
+                    var mon = section.Secheduals.MON ? "x" : "";
+                    var tue = section.Secheduals.TUE ? "x" : "";
+                    var wed = section.Secheduals.WED ? "x" : "";
+                    var the = section.Secheduals.THE ? "x" : "";
+                    var fri = section.Secheduals.FRI ? "x" : "";
+                    var sat = section.Secheduals.SAT ? "x" : "";
+
+                    Console.WriteLine($"{section.Id} --- {section.SectionName} ---- {section.Course.CourseName} --- {section.Instractor.FName} {section.Instractor.LName} --- {section.Secheduals.Title} --- {section.TimeSlot} --- {sun} - {mon} - {tue} - {wed} - {the} - {fri} - {sat} ");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
